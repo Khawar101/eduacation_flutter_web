2d46d03ec133b9108475873cd57dc6607395f592
mudassir060 <mudassirmukhtar4@gmail.com>
2023-06-13 19:01:05 +0500

    connected stacked

--- lib/ui/views/courses/upload_courses/upload_courses_viewmodel.dart
+++ lib/ui/views/courses/upload_courses/upload_courses_viewmodel.dart
@@ -1,16 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-
 import 'upload_view_1.dart';
+import 'upload_view_2.dart';
+import 'upload_view_3.dart';
+import 'upload_view_4.dart';
+import 'upload_view_5.dart';
 
 class UploadCoursesViewModel extends BaseViewModel {
   var screenNo = 0;
   var screens = [
     const UploadView_1(),
-    const Text("2 page"),
-    const Text("page3"),
-    const Text("page8"),
-    const Text("page7"),
+    const UploadView_2(),
+    const UploadView_3(),
+    const UploadView_4(),
+    const UploadView_5(),
   ];
 
   backPage() {
