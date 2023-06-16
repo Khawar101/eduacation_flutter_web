1b27eb53f93949551a3be22d3a7281ec5a769a4d
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-06-16 11:46:06 +0500

    library changes

--- lib/ui/views/auth/signup/signup_view.dart
+++ lib/ui/views/auth/signup/signup_view.dart
@@ -22,175 +22,183 @@ class SignupView extends StackedView<SignupViewModel> {
     return Scaffold(
         backgroundColor: Theme.of(context).colorScheme.background,
         body: SingleChildScrollView(
-          child: Container(
-            // height: MediaQuery.of(context).size.height * 1,
-            width: MediaQuery.of(context).size.width * 0.5,
-            color: Colors.white,
-            child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: MediaQuery.of(context).size.width * 0.09,
-                  vertical: MediaQuery.of(context).size.height * 0.15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const CustomText(
-                      text: 'Create an account',
-                      size: 18,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.black),
-                  const SizedBox(height: 18),
-                  const CustomText(
-                      text: 'Please enter your name,email or password',
-                      size: 14,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black45),
-                  const SizedBox(height: 24),
-                  CustomTextField(
-                    hintText: 'Name',
-                  ),
-                  const SizedBox(height: 18),
-                  CustomTextField(
-                    hintText: 'you@example.com',
-                    prefix: const Icon(Icons.email),
-                    validator: (value) {
-                      // adminEmail= value!;
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 18),
-                  CustomTextField(
-                    hintText: 'At least 8 characters',
-                    validator: (value) {
-                      return null;
-                    },
-                    isObscureText: viewModel.visibleCheck,
-                    prefix: GestureDetector(
-                      onTap: viewModel.visible_check,
-                      child: Icon(
-                        viewModel.visibleCheck == true
-                            ? Icons.visibility_off_outlined
-                            : Icons.visibility_outlined,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  GestureDetector(
-                    onTap: () {
-                      // allowAdminToLogin();
-                    },
-                    child: Container(
-                      height: 50,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: Colors.black54,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: const Center(
-                          child: CustomText(
-                              text: 'Create account',
-                              size: 14,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white)),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  const Row(
+          child: Row(
+            children: [
+              Container(
+                // height: MediaQuery.of(context).size.height * 1,
+                width: MediaQuery.of(context).size.width * 0.5,
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.09,
+                      vertical: MediaQuery.of(context).size.height * 0.15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                        child: Divider(
-                          color: Colors.black12,
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      CustomText(
-                          text: 'or',
+                      const CustomText(
+                          text: 'Create an account',
+                          size: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black),
+                      const SizedBox(height: 18),
+                      const CustomText(
+                          text: 'Please enter your name,email or password',
                           size: 14,
                           fontWeight: FontWeight.w400,
-                          color: Colors.black54),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: Divider(
-                          color: Colors.black12,
+                          color: Colors.black45),
+                      const SizedBox(height: 24),
+                      CustomTextField(
+                        hintText: 'Name',
+                      ),
+                      const SizedBox(height: 18),
+                      CustomTextField(
+                        hintText: 'you@example.com',
+                        prefix: const Icon(Icons.email),
+                        validator: (value) {
+                          // adminEmail= value!;
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 18),
+                      CustomTextField(
+                        hintText: 'At least 8 characters',
+                        validator: (value) {
+                          return null;
+                        },
+                        isObscureText: viewModel.visibleCheck,
+                        prefix: GestureDetector(
+                          onTap: viewModel.visible_check,
+                          child: Icon(
+                            viewModel.visibleCheck == true
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                            color: Colors.black,
+                          ),
                         ),
                       ),
+                      const SizedBox(height: 30),
+                      GestureDetector(
+                        onTap: () {
+                          // allowAdminToLogin();
+                        },
+                        child: Container(
+                          height: 50,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: const Center(
+                              child: CustomText(
+                                  text: 'Create account',
+                                  size: 14,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white)),
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      const Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.black12,
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          CustomText(
+                              text: 'or',
+                              size: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black54),
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.black12,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            height: 50,
+                            width: MediaQuery.of(context).size.width * 0.15,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black12),
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'icons/google-48.png',
+                                  scale: 2,
+                                ),
+                                const SizedBox(width: 6),
+                                Visibility(
+                                  visible: screenWidth >= 700,
+                                  child: const CustomText(
+                                      text: 'Google',
+                                      size: 14,
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: 50,
+                            width: MediaQuery.of(context).size.width * 0.15,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black12),
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image.asset(
+                                  'icons/facebook-48.png',
+                                  scale: 2,
+                                ),
+                                const SizedBox(width: 6),
+                                Visibility(
+                                  visible: screenWidth >= 700,
+                                  child: const CustomText(
+                                      text: 'Facebook',
+                                      size: 14,
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      verticalSpaceSmall,
+                      Align(
+                        alignment: Alignment.center,
+                        child: RichText(
+                            text: TextSpan(children: [
+                          TextSpan(
+                              text: "Already have an account? ",
+                              style: GoogleFonts.ibmPlexSans(
+                                  color: Colors.black54, fontSize: 12)),
+                          TextSpan(
+                              text: " Sign in",
+                              style: GoogleFonts.ibmPlexSans(
+                                  color: Colors.lightBlue,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w500))
+                        ])),
+                      )
                     ],
                   ),
-                  const SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        height: 50,
-                        width: MediaQuery.of(context).size.width * 0.15,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black12),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'icons/google-48.png',
-                              scale: 2,
-                            ),
-                            const SizedBox(width: 6),
-                            Visibility(
-                              visible: screenWidth >= 700,
-                              child: const CustomText(
-                                  text: 'Google',
-                                  size: 14,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.black),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        height: 50,
-                        width: MediaQuery.of(context).size.width * 0.15,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black12),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(
-                              'icons/facebook-48.png',
-                              scale: 2,
-                            ),
-                            const SizedBox(width: 6),
-                            Visibility(
-                              visible: screenWidth >= 700,
-                              child: const CustomText(
-                                  text: 'Facebook',
-                                  size: 14,
-                                  fontWeight: FontWeight.w700,
-                                  color: Colors.black),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                  verticalSpaceSmall,
-                  Align(
-                    alignment: Alignment.center,
-                    child: RichText(
-                        text: TextSpan(children: [
-                      TextSpan(
-                          text: "Already have an account? ",
-                          style: GoogleFonts.ibmPlexSans(
-                              color: Colors.black54, fontSize: 12)),
-                      TextSpan(
-                          text: " Sign in",
-                          style: GoogleFonts.ibmPlexSans(
-                              color: Colors.lightBlue,
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500))
-                    ])),
-                  )
-                ],
+                ),
               ),
-            ),
+              Container(
+                 width: MediaQuery.of(context).size.width * 0.5,
+                color: Colors.black,
+              )
+            ],
           ),
         ));
   }
