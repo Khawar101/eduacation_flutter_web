6dad0ad63e40bc7dc7f412c8bf5921ff95b6845b
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-06-21 11:10:33 +0500

    validatin

--- lib/ui/views/courses/upload_courses/widgets/card.dart
+++ lib/ui/views/courses/upload_courses/widgets/card.dart
@@ -10,7 +10,7 @@ import '../../../../../services/Model/CoursesModel.dart';
 
 Widget Card_Page(context, lectureData) {
   return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+    padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
     child: Row(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -25,7 +25,7 @@ Widget Card_Page(context, lectureData) {
               borderRadius: BorderRadius.circular(10),
               child: Image.network(
                 lectureData.thumbnail.toString(),
-                height: MediaQuery.of(context).size.height * 0.24,
+                height: MediaQuery.of(context).size.height * 0.3,
                 fit: BoxFit.cover,
               )),
         ),
@@ -33,34 +33,33 @@ Widget Card_Page(context, lectureData) {
         Expanded(
           flex: 2,
           //     MediaQuery.of(context).size.width*0.12,
-          child: Padding(
-            padding: const EdgeInsets.all(0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ButtonText(text: lectureData.title.toString(), color: Colors.black),
-                    IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.remove_circle_outline))
-                  ],
-                ),
-                verticalSpaceSmall,
-                Text(
-                  lectureData.description.toString(),
-                  style: const TextStyle(
-                      color: Color(0xff969799),
-                      fontSize: 15,
-                      overflow: TextOverflow.ellipsis,
-                      fontWeight: FontWeight.w400),
-                  maxLines: 6,
-                ),
-                verticalSpaceTiny,
-              ],
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width:MediaQuery.of(context).size.width*0.11,
+                    child: ButtonText(text: lectureData.title.toString(), color: Colors.black,)),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.remove_circle_outline))
+                ],
+              ),
+              verticalSpaceSmall,
+              Text(
+                lectureData.description.toString(),
+                style: const TextStyle(
+                    color: Color(0xff969799),
+                    fontSize: 15,
+                    overflow: TextOverflow.ellipsis,
+                    fontWeight: FontWeight.w400),
+                maxLines: 6,
+              ),
+           
+            ],
           ),
         )
       ],
