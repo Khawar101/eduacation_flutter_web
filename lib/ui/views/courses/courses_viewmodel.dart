0bbc5339049c0e7745f1e6612e8d64ee23c59993
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-06-21 15:54:11 +0500

    up

--- lib/ui/views/courses/courses_viewmodel.dart
+++ lib/ui/views/courses/courses_viewmodel.dart
@@ -1,10 +1,11 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, unused_field
 
 import 'dart:developer';
 import 'package:education_flutter_web/ui/views/courses/upload_courses/widgets/pop_up_menu.dart';
 import 'package:education_flutter_web/utils/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 import '../../../app/app.locator.dart';
 import '../../../services/Model/CoursesModel.dart';
@@ -14,6 +15,7 @@ import '../../widgets/common/sized_text/sized_text.dart';
 class CoursesViewModel extends BaseViewModel {
   // final _navigationService = locator<NavigationService>();
   final coursesService = locator<CoursesService>();
+
 
   var pageNo = 0;
   nextPage() {
