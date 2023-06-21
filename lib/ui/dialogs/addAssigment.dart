990339f87dcfa3533b0ece72d1fd60d450952ee2
mudassir060 <mudassirmukhtar4@gmail.com>
2023-06-21 15:31:52 +0500

    validation

--- lib/ui/dialogs/addAssigment.dart
+++ lib/ui/dialogs/addAssigment.dart
@@ -16,53 +16,57 @@ addAssigmentAlert(
     CoursesModel courseData,
     addThumbnail,
     addAssigmentFile) async {
+  var _formKey = GlobalKey<FormState>();
+
   return showDialog<String>(
     context: context,
     builder: (BuildContext context) => AlertDialog(
-      title:const Center(
-        child:  Text(
+      title: const Center(
+        child: Text(
           'Upload Lecture',
         ),
       ),
       content: StatefulBuilder(builder: (context, newSetState) {
-        return SizedBox(
-        height: 290,
-         width: MediaQuery.of(context).size.width*0.3,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconTextField(
-                titleText: "Title",
-                controller: titleCtrl,
-                hintText: 'e.g: Free Programming Courses',
-              ),
-              IconTextField(
-                titleText: "Description",
-                controller: descriptionCtrl,
-                hintText: 'e.g: Free Programming Courses',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  addBtn("Thumbnail", _coursesService.progressshow,
-                      _coursesService.thubnailUrl, () {
-                    addThumbnail(newSetState);
-                  },context),
-                  const SizedBox(width: 20),
-                  addBtn("Assigment", _coursesService.videoProgress,
-                      _coursesService.assigmentUrl, () {
-                    addAssigmentFile(newSetState);
-                  },context),
-                ],
-              )
-            ],
+        return Form(
+          key: _formKey,
+          child: SizedBox(
+            height: 290,
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconTextField(
+                  titleText: "Title",
+                  controller: titleCtrl,
+                  hintText: 'e.g: Free Programming Courses',
+                ),
+                IconTextField(
+                  titleText: "Description",
+                  controller: descriptionCtrl,
+                  hintText: 'e.g: Free Programming Courses',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    addBtn("Thumbnail", _coursesService.progressshow,
+                        _coursesService.thubnailUrl, () {
+                      addThumbnail(newSetState);
+                    }, context),
+                    const SizedBox(width: 20),
+                    addBtn("Assigment", _coursesService.videoProgress,
+                        _coursesService.assigmentUrl, () {
+                      addAssigmentFile(newSetState);
+                    }, context),
+                  ],
+                )
+              ],
+            ),
           ),
         );
       }),
-      
       actions: <Widget>[
         TextButton(
           onPressed: () => Navigator.pop(
@@ -73,22 +77,25 @@ addAssigmentAlert(
         ),
         TextButton(
           onPressed: () {
-
-            assigments.add(Assigment(
-              title: titleCtrl.text,
-              description: descriptionCtrl.text,
-              thumbnail: _coursesService.thubnailUrl,
-              fileUrl: _coursesService.assigmentUrl,
-            ));
-            courseData.assigment = assigments;
-            notifyListeners();
-            titleCtrl.clear();
-            descriptionCtrl.clear();
-            _coursesService.thubnailUrl = "";
-            _coursesService.assigmentUrl = "";
-            _coursesService.progressshow = 0;
-            _coursesService.videoProgress = 0;
-            Navigator.pop(context);
+            final isValid = _formKey.currentState?.validate();
+            if (isValid!) {
+              assigments.add(Assigment(
+                title: titleCtrl.text,
+                description: descriptionCtrl.text,
+                thumbnail: _coursesService.thubnailUrl,
+                fileUrl: _coursesService.assigmentUrl,
+              ));
+              courseData.assigment = assigments;
+              notifyListeners();
+              titleCtrl.clear();
+              descriptionCtrl.clear();
+              _coursesService.thubnailUrl = "";
+              _coursesService.assigmentUrl = "";
+              _coursesService.progressshow = 0;
+              _coursesService.videoProgress = 0;
+              Navigator.pop(context);
+            }
+            _formKey.currentState?.save();
           },
           child: const Text(
             'Add Assigment',
--- lib/ui/dialogs/addLecture.dart
+++ lib/ui/dialogs/addLecture.dart
@@ -15,50 +15,53 @@ addLectureAlert(
     CoursesModel courseData,
     addThumbnail,
     addVideo) async {
+  var _formKey = GlobalKey<FormState>();
+
   return showDialog<String>(
     context: context,
     builder: (BuildContext context) => AlertDialog(
-   
       title: const Text(
         'Upload Lecture',
       ),
       content: StatefulBuilder(builder: (context, newSetState) {
-        return Container(
-        height: 290,
-         width: MediaQuery.of(context).size.width*0.3,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconTextField(
-               
-                titleText: "Title",
-                controller: videoTitleCtrl,
-                hintText: 'e.g: Free Programming Courses',
-              ),
-              IconTextField(
-                titleText: "Description",
-                controller: videoDescriptionCtrl,
-                hintText: 'e.g: Free Programming Courses',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  addBtn("Thumbnail", _coursesService.progressshow,
-                      _coursesService.thubnailUrl, () {
-                    addThumbnail(newSetState);
-                  },context),
-                  const SizedBox(width: 20),
-                  addBtn("Video", _coursesService.videoProgress,
-                      _coursesService.videoUrl, () {
-                    addVideo(newSetState);
-                  },context),
-                  // dropAddBtn(),
-                ],
-              )
-            ],
+        return Form(
+          key: _formKey,
+          child: Container(
+            height: 290,
+            width: MediaQuery.of(context).size.width * 0.3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconTextField(
+                  titleText: "Title",
+                  controller: videoTitleCtrl,
+                  hintText: 'e.g: Free Programming Courses',
+                ),
+                IconTextField(
+                  titleText: "Description",
+                  controller: videoDescriptionCtrl,
+                  hintText: 'e.g: Free Programming Courses',
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    addBtn("Thumbnail", _coursesService.progressshow,
+                        _coursesService.thubnailUrl, () {
+                      addThumbnail(newSetState);
+                    }, context),
+                    const SizedBox(width: 20),
+                    addBtn("Video", _coursesService.videoProgress,
+                        _coursesService.videoUrl, () {
+                      addVideo(newSetState);
+                    }, context),
+                    // dropAddBtn(),
+                  ],
+                )
+              ],
+            ),
           ),
         );
       }),
@@ -72,21 +75,25 @@ addLectureAlert(
         ),
         TextButton(
           onPressed: () {
-            lectures.add(Lecture(
-              title: videoTitleCtrl.text,
-              description: videoDescriptionCtrl.text,
-              thumbnail: _coursesService.thubnailUrl,
-              videoUrl: _coursesService.videoUrl,
-            ));
-            courseData.lecture = lectures;
-            notifyListeners();
-            videoTitleCtrl.clear();
-            videoDescriptionCtrl.clear();
-            _coursesService.thubnailUrl = "";
-            _coursesService.videoUrl = "";
-            _coursesService.progressshow = 0;
-            _coursesService.videoProgress = 0;
-            Navigator.pop(context);
+            final isValid = _formKey.currentState?.validate();
+            if (isValid!) {
+              lectures.add(Lecture(
+                title: videoTitleCtrl.text,
+                description: videoDescriptionCtrl.text,
+                thumbnail: _coursesService.thubnailUrl,
+                videoUrl: _coursesService.videoUrl,
+              ));
+              courseData.lecture = lectures;
+              notifyListeners();
+              videoTitleCtrl.clear();
+              videoDescriptionCtrl.clear();
+              _coursesService.thubnailUrl = "";
+              _coursesService.videoUrl = "";
+              _coursesService.progressshow = 0;
+              _coursesService.videoProgress = 0;
+              Navigator.pop(context);
+            }
+            _formKey.currentState?.save();
           },
           child: const Text(
             'Add Lecture',
