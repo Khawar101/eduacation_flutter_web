059e32c7beeecf7d91f22cd7390a52abf22a1a17
mudassir060 <mudassirmukhtar4@gmail.com>
2023-06-22 13:26:37 +0500

    course Card widget

--- lib/ui/views/courses/widgets/coursesBuilder.dart
+++ lib/ui/views/courses/widgets/coursesBuilder.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-
 import '../../../../services/Model/CoursesModel.dart';
 import '../../../../services/courses_service.dart';
 import '../../../../utils/loading.dart';
-import '../../../widgets/common/sized_text/sized_text.dart';
-import '../upload_courses/widgets/pop_up_menu.dart';
+import 'coursesCard.dart';
 
 Widget coursesBuilder(CoursesService coursesService) {
   return StreamBuilder<List<CoursesModel>>(
@@ -28,75 +26,8 @@ Widget coursesBuilder(CoursesService coursesService) {
           mainAxisSpacing: 8,
         ),
         itemBuilder: (BuildContext context, int index) {
-          var data = snapshot.data![index];
-          return GestureDetector(
-            child: Card(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.network(
-                    data.coverPic.toString(),
-                    fit: BoxFit.fitWidth,
-                    height: 130,
-                    width: 300,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              left: 8.0,
-                              top: 8.0,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data.title.toString(),
-                                  style: const TextStyle(
-                                      overflow: TextOverflow.ellipsis,
-                                      fontWeight: FontWeight.bold),
-                                  maxLines: 2,
-                                ),
-
-                                // reating(data["rating"], data["reated"]),
-                                CustomText(
-                                    text: "\$${data.price.toString()}",
-                                    size: 17,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black),
-                              ],
-                            ),
-                          ),
-                          const Positioned(
-                            right: 5,
-                            top: 5,
-                            child: PopupMenu(),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 4),
-                        child: Text(
-                          data.description.toString(),
-                          style: const TextStyle(
-                              color: Colors.black45,
-                              overflow: TextOverflow.ellipsis,
-                              fontWeight: FontWeight.bold),
-                          maxLines: 2,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          );
+          CoursesModel data = snapshot.data![index];
+          return courseCard(data);
         },
       );
     },
---
+++ lib/ui/views/courses/widgets/coursesCard.dart
@@ -0,0 +1,77 @@
+import 'package:education_flutter_web/services/Model/CoursesModel.dart';
+import 'package:flutter/material.dart';
+
+import '../../../widgets/common/sized_text/sized_text.dart';
+import '../upload_courses/widgets/pop_up_menu.dart';
+
+Widget courseCard(CoursesModel data){
+  return GestureDetector(
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    data.coverPic.toString(),
+                    fit: BoxFit.fitWidth,
+                    height: 130,
+                    width: 300,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 8.0,
+                              top: 8.0,
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  data.title.toString(),
+                                  style: const TextStyle(
+                                      overflow: TextOverflow.ellipsis,
+                                      fontWeight: FontWeight.bold),
+                                  maxLines: 2,
+                                ),
+
+                                // reating(data["rating"], data["reated"]),
+                                CustomText(
+                                    text: "\$${data.price.toString()}",
+                                    size: 17,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ],
+                            ),
+                          ),
+                          const Positioned(
+                            right: 5,
+                            top: 5,
+                            child: PopupMenu(),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8, vertical: 4),
+                        child: Text(
+                          data.description.toString(),
+                          style: const TextStyle(
+                              color: Colors.black45,
+                              overflow: TextOverflow.ellipsis,
+                              fontWeight: FontWeight.bold),
+                          maxLines: 2,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+       
+}
