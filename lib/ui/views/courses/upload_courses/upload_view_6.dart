25d0c19f614b2890dec30967c4421fd5b6862a33
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-06-23 17:31:27 +0500

    valid

--- lib/ui/views/courses/upload_courses/upload_view_6.dart
+++ lib/ui/views/courses/upload_courses/upload_view_6.dart
@@ -4,7 +4,9 @@ import 'package:education_flutter_web/ui/common/ui_helpers.dart';
 import 'package:education_flutter_web/ui/views/courses/upload_courses/upload_view_2.dart';
 import 'package:education_flutter_web/ui/views/courses/upload_courses/upload_view_4.dart';
 import 'package:education_flutter_web/ui/views/courses/upload_courses/upload_view_5.dart';
+import 'package:education_flutter_web/ui/views/courses/upload_courses/widgets/rating_cont.dart';
 import 'package:education_flutter_web/ui/widgets/common/sized_text/sized_text.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'upload_courses_viewmodel.dart';
@@ -18,78 +20,103 @@ class CourseDetails extends StackedView<UploadCoursesViewModel> {
     UploadCoursesViewModel viewModel,
     Widget? child,
   ) {
-    // var width = MediaQuery.of(context).size.width;
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              BigText(
-                  text: viewModel.courseData.title.toString(),
-                  color: Colors.black),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Row(
+     return SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                BigText(
+                    text: viewModel.courseData.title.toString(),
+                    color: Colors.black),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Row(
+                    children: [
+                      const ButtonText(text: "Price:", color: Colors.black),
+                      SmallText(
+                          text: " ${viewModel.courseData.price}\$ ",
+                          color: Colors.black),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            verticalSpaceSmall,
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const ButtonText(text: "Price:", color: Colors.black),
+                    const ButtonText(text: "Catogery:", color: Colors.black),
                     SmallText(
-                        text: " ${viewModel.courseData.price}\$ ",
+                        text: viewModel.courseData.category.toString(),
                         color: Colors.black),
                   ],
                 ),
+                horizontalSpaceLarge,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const ButtonText(text: "Duration:", color: Colors.black),
+                    SmallText(
+                        text: viewModel.courseData.duration.toString(),
+                        color: Colors.black),
+                  ],
+                ),
+              ],
+            ),
+            verticalSpaceSmall,
+            const ButtonText(text: "Description:", color: Colors.black),
+            SmallText(
+                color: Colors.black,
+                text: viewModel.courseData.description.toString()),
+            verticalSpaceSmall,
+            const StarText(
+              text: 'Lectures',
+            ),
+            const UploadView_4(),
+            verticalSpaceSmall,
+            const StarText(
+              text: 'Assignments',
+            ),
+            const UploadView_5(),
+            verticalSpaceSmall,
+            const StarText(
+              text: 'FAQ',
+            ),
+            const UploadView_2(),
+            verticalSpaceSmall,
+            Container(
+              height: 150,
+              child: ScrollConfiguration(
+                behavior:MyCustomScrollBehavior(),
+                child: ListView.builder(
+                    itemCount: 500,
+                    // shrinkWrap: true,
+                    // physics: ClampingScrollPhysics(),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 100, color: Colors.red,
+                          // width: MediaQuery.of(context).size.width*1,
+                          child: Text("djkdjddd"),
+                        ),
+                      );
+                    }),
               ),
-            ],
-          ),
-          verticalSpaceSmall,
-          Row(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const ButtonText(text: "Catogery:", color: Colors.black),
-                  SmallText(
-                      text: viewModel.courseData.category.toString(),
-                      color: Colors.black),
-                ],
-              ),
-              horizontalSpaceLarge,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const ButtonText(text: "Duration:", color: Colors.black),
-                  SmallText(
-                      text: viewModel.courseData.duration.toString(),
-                      color: Colors.black),
-                ],
-              ),
-            ],
-          ),
-          verticalSpaceSmall,
-          const ButtonText(text: "Description:", color: Colors.black),
-          SmallText(
-              color: Colors.black,
-              text: viewModel.courseData.description.toString()),
-          verticalSpaceSmall,
-          const StarText(
-            text: 'Lectures',
-          ),
-          const UploadView_4(),
-          verticalSpaceSmall,
-          const StarText(
-            text: 'Assignments',
-          ),
-          const UploadView_5(),
-          verticalSpaceSmall,
-          const StarText(
-            text: 'FAQ',
-          ),
-          const UploadView_2(),
-          verticalSpaceSmall,
-        ],
-      ),
+            ),
+     
+            SizedBox(
+              height: 60,
+            )
+          ],
+        ),
+      // ),
     );
   }
 
@@ -99,3 +126,12 @@ class CourseDetails extends StackedView<UploadCoursesViewModel> {
   ) =>
       UploadCoursesViewModel();
 }
+ class MyCustomScrollBehavior extends MaterialScrollBehavior {
+      // Override behavior methods and getters like dragDevices
+      @override
+      Set<PointerDeviceKind> get dragDevices => { 
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
+    }
---
+++ lib/ui/views/courses/upload_courses/widgets/rating_cont.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Widget ratingCont(context) {
+  return Container(
+    height: 100,
+    width: MediaQuery.of(context).size.width*0.2,
+    color: Colors.red,
+    child: Text("hggggggggggggggggggggggggggggggggg"),
+  );
+}
