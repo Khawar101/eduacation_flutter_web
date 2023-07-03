9860f745f23b0260d7627b016bd4f317a4332eeb
Rehan Ullah <rehan.gee888@gmail.com>
2023-07-03 17:41:18 +0500

    worked on accounts page

--- lib/ui/views/account/account_view.dart
+++ lib/ui/views/account/account_view.dart
@@ -1,3 +1,6 @@
+import 'package:education_flutter_web/ui/views/account/widget/income_tab.dart';
+import 'package:education_flutter_web/ui/views/account/widget/statistics.dart';
+import 'package:education_flutter_web/ui/views/account/widget/transactions_view.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
@@ -12,10 +15,50 @@ class AccountView extends StackedView<AccountViewModel> {
     AccountViewModel viewModel,
     Widget? child,
   ) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.9,
+      color: Colors.white,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: screenWidth,
+                height: screenWidth * 0.15,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.background,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                ),
+                child: const Row(
+                  children: [
+                    Statistics(
+                      heading: "Education",
+                      money: "350.00",
+                      iconss: Icons.cast_for_education_outlined,
+                    ),
+                    Statistics(
+                      heading: "Travel",
+                      money: "850.00",
+                      iconss: Icons.map_outlined,
+                    ),
+                    Statistics(
+                      heading: "Shopping",
+                      money: "420.00",
+                      iconss: Icons.shopping_bag_outlined,
+                    ),
+                  ],
+                ),
+              ),
+              const Income_Tab(),
+              Transactions_View(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/ui/views/account/widget/transactions_view.dart
@@ -0,0 +1,85 @@
+import 'package:education_flutter_web/ui/views/account/widget/transactions.dart';
+import 'package:education_flutter_web/ui/widgets/common/sized_text/sized_text.dart';
+import 'package:flutter/material.dart';
+
+const List<String> list = ['All', 'Sent', 'Received'];
+
+class Transactions_View extends StatefulWidget {
+  const Transactions_View({super.key});
+
+  @override
+  State<Transactions_View> createState() => _Transactions_ViewState();
+}
+
+class _Transactions_ViewState extends State<Transactions_View> {
+  String dropdownValue = list.first;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CustomText(
+                text: "Transactions",
+                size: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Container(
+                height: 35,
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      style: BorderStyle.solid,
+                      color: Colors.black38,
+                    ),
+                    borderRadius: const BorderRadius.all(Radius.circular(30))),
+                child: DropdownButton(
+                  borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  value: dropdownValue,
+                  icon: const Icon(
+                    Icons.keyboard_arrow_down,
+                    color: Colors.blue,
+                  ),
+                  elevation: 0,
+                  style: const TextStyle(color: Colors.black),
+                  underline: Container(
+                    height: 0,
+                  ),
+                  onChanged: (String? value) {
+                    // This is called when the user selects an item.
+                    setState(() {
+                      dropdownValue = value!;
+                    });
+                  },
+                  items: list.map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 5),
+          child: CustomText(
+              text: "Today",
+              size: 14,
+              fontWeight: FontWeight.normal,
+              color: Colors.black45),
+        ),
+        Transaction(sign: "S", title: "P&B", purpose: "Shopping", amount: "-\$199.00",),
+        Transaction(sign: "R", title: "From Ann Jones", purpose: "Shopping", amount: "-\$199.00",),
+        Transaction(sign: "S", title: "P&B", purpose: "Shopping", amount: "-\$199.00",),
+      ],
+    );
+  }
+}
