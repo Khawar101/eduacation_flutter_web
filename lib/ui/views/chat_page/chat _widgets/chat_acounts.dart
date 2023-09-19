4c8fbfc3c985f3be92cf3f83032daafe1525f4cc
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-09-19 14:49:10 +0500

    chat accounrt

--- lib/ui/views/chat_page/chat _widgets/chat_acounts.dart
+++ lib/ui/views/chat_page/chat _widgets/chat_acounts.dart
@@ -111,12 +111,7 @@ class ChatUsers extends ViewModelWidget<ChatPageViewModel> {
               );
             },
           ),
-          // InkWell
-          // (onTap: () {
-          //      viewModel.setOnlineStatus("online");
-          // },
-            
-          //   child: Container(height: 70,width:50,color: Colors.black,))
+          
         ],
       ),
     );
--- lib/ui/views/chat_page/chat _widgets/user_inbox.dart
+++ lib/ui/views/chat_page/chat _widgets/user_inbox.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:education_flutter_web/utils/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import '../../../../services/Model/userData.dart';
@@ -10,14 +9,12 @@ import 'chat_message.dart';
 class UserInbox extends ViewModelWidget<ChatPageViewModel> {
   final String chatId;
   final String uID;
-  const UserInbox({
-    Key? key,
-    required this.chatId,required this.uID
-  }) : super(key: key);
+  const UserInbox({Key? key, required this.chatId, required this.uID})
+      : super(key: key);
   @override
   Widget build(BuildContext context, ChatPageViewModel viewModel) {
-    // double height = MediaQuery.of(context).size.height;
-    return Column(
+    return  uID==""?Container():
+     Column(
       children: [
         SizedBox(
           height: 50,
@@ -41,61 +38,40 @@ class UserInbox extends ViewModelWidget<ChatPageViewModel> {
                   const SizedBox(
                     height: 1,
                   ),
+                  StreamBuilder(
+                    stream: viewModel.publisherStream(uID),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('Something went wrong');
+                      }
 
-                 StreamBuilder(
-      stream: viewModel. publisherStream(uID),
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasError) {
-          return const Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Loading(20);
-        }
-        userData _userData = userData.fromJson(snapshot.data.data());
-        return             Row(
-                    children: [
-                      Icon(
-                        Icons.circle,
-                        color:_userData.status??false
-                            ? Colors.green
-                            : Colors.grey,
-                        size: 11,
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                       _userData.status??false
-                            ? "Active now"
-                            : "Offline",
-                        style: const TextStyle(fontSize: 10),
-                      )
-                    ],
-                  );
-      },
-    )
-
-                  //               Row(
-                  //   children: [
-                  //     Icon(
-                  //       Icons.circle,
-                  //       color: viewModel.isOnline
-                  //           ? Colors.green
-                  //           : Colors.grey,
-                  //       size: 11,
-                  //     ),
-                  //     const SizedBox(
-                  //       width: 8,
-                  //     ),
-                  //     Text(
-                  //       viewModel.isOnline
-                  //           ? "Active now"
-                  //           : "Offline",
-                  //       style: const TextStyle(fontSize: 10),
-                  //     )
-                  //   ],
-                  // ),
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Container();
+                      }
+                      userData _userData =
+                          userData.fromJson(snapshot.data.data());
+                      return Row(
+                        children: [
+                          Icon(
+                            Icons.circle,
+                            color: _userData.status ?? false
+                                ? Colors.green
+                                : Colors.grey,
+                            size: 11,
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          Text(
+                            _userData.status ?? false
+                                ? "Active now"
+                                : "Offline",
+                            style: const TextStyle(fontSize: 10),
+                          )
+                        ],
+                      );
+                    },
+                  )
                 ],
               ),
             ],
@@ -191,227 +167,3 @@ class UserInbox extends ViewModelWidget<ChatPageViewModel> {
     );
   }
 }
-
-
-// import 'dart:developer';
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:education_flutter_web/app/app.locator.dart';
-// import 'package:education_flutter_web/services/login_service.dart';
-// import 'package:education_flutter_web/ui/views/chat_page/chat%20_widgets/chat_message.dart';
-// import 'package:education_flutter_web/ui/views/chat_page/chat_page_viewmodel.dart';
-// import 'package:flutter/material.dart';
-// import 'package:stacked/stacked.dart';
-
-// class ChatRoom extends StatefulWidget {
-//     final String chatId;
-//   const ChatRoom({super.key, required this.chatId});
-
-//   @override
-//   State<ChatRoom> createState() => _ChatRoomState();
-// }
-
-// class _ChatRoomState extends State<ChatRoom> with WidgetsBindingObserver  {
-//    final loginService = locator<LoginService>();
-//  final  FirebaseFirestore firestore = FirebaseFirestore.instance;
-//   @override
-//     void initState() {
-//     super.initState();
-//     WidgetsBinding.instance.addObserver(this);
-//       setStatus("online");setStatus("offline");
-//   }
-//   void setStatus(String status) async{
-//      await  firestore.collection('users').doc(loginService.UserData.uID).update({
-//   "status":status
-//   }
-// );
-//   }
-
-
-//   @override
-//   void didChangeAppLifecycleState(AppLifecycleState state){
-//     // if(state == AppLifecycleState.resumed){
-//     //  setStatus("online");
-//     //  log("onlne");
-//     // }else{
-//     //   setStatus("offline");
-//     //   log("offline");
-//     // }
-//      switch (state) {
-//       case AppLifecycleState.resumed:
-//         log('On Resume');
-//         setStatus("online");
-//         break;
-//       case AppLifecycleState.inactive:
-//         log('On inactive');
-//          setStatus("offline");
-//         break;
-//       case AppLifecycleState.paused:
-//         log('On paused');
-//            setStatus("offline");
-//         break;
-//       case AppLifecycleState.detached:
-//         log('On detached');
-//            setStatus("offline");
-//         break;
-//       case AppLifecycleState.hidden:
-//         log('On hidden');   setStatus("offline");
-//         break;
-//     }
-//   }
-//  @override
-//   void dispose() {
-
-//      WidgetsBinding.instance.removeObserver(this);
-//     setStatus("offline");
-//      super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return  ViewModelBuilder<ChatPageViewModel>.nonReactive(
-//       builder: (context, viewModel, child) =>
-//        Column(
-//       children: [
-//         SizedBox(
-//           height: 50,
-//           child: Row(
-//             crossAxisAlignment: CrossAxisAlignment.center,
-//             children: [
-//               const SizedBox(width: 20),
-//               CircleAvatar(
-//                 backgroundColor: Colors.red,
-//                 backgroundImage: NetworkImage(viewModel.profile),
-//               ),
-//               const SizedBox(width: 10),
-//               Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 crossAxisAlignment: CrossAxisAlignment.start,
-//                 children: [
-//                   Text(
-//                     viewModel.name.toString(),
-//                     style: const TextStyle(fontSize: 20),
-//                   ),
-//                   const SizedBox(
-//                     height: 1,
-//                   ),
-//                   Row(
-//       children: [
-//         Icon(
-//           Icons.circle,
-//           color: viewModel.isOnline
-//               ? Colors.green
-//               : Colors.grey,
-//           size: 11,
-//         ),
-//         const SizedBox(
-//           width: 8,
-//         ),
-//         Text(
-//           viewModel.isOnline
-//               ? "Active now"
-//               : "Offline",
-//           style: const TextStyle(fontSize: 10),
-//         )
-//       ],
-//     ),
- 
-
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ),
-//         SizedBox(
-//           height: MediaQuery.of(context).size.height - 160,
-//           child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-//               stream: viewModel.getMessagesStream(),
-//               builder: (context, snapshot) {
-//                 if (snapshot.hasError) {
-//                   log(snapshot.error.toString());
-//                   return const Text('Error fetching messages');
-//                 }
-
-//                 if (!snapshot.hasData) {
-//                   return const Text('No messages yet');
-//                 }
-//                 return ListView.builder(
-//                   itemCount: snapshot.data?.docs.length,
-//                   reverse: true,
-//                   itemBuilder: (context, index) {
-//                     var messageData = snapshot.data!.docs[index].data();
-
-//                     return Padding(
-//                       padding: const EdgeInsets.all(8.0),
-//                       child: MessageBubble(
-//                         isMe: messageData['UID'] ==
-//                             viewModel.loginService.UserData.uID,
-//                         message: messageData['SMS'],
-//                       ),
-//                     );
-//                   },
-//                 );
-//               }),
-//         ),
-//         Container(
-//           padding: const EdgeInsets.symmetric(horizontal: 8.0),
-//           decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(10),
-//             border: Border.all(
-//               color: const Color(0xff4873a6).withOpacity(0.7),
-//             ),
-//             color: Colors.grey[200],
-//           ),
-//           child: Row(
-//             crossAxisAlignment: CrossAxisAlignment.end,
-//             children: [
-//               IconButton(
-//                   onPressed: () {
-//                     // log(viewModel.checkOnlineStatus.toString());
-//                   },
-//                   icon: Icon(
-//                     Icons.emoji_emotions,
-//                     color: viewModel.isTextEmpty
-//                         ? Colors.grey
-//                         : const Color(0xff4873a6).withOpacity(0.7),
-//                   )),
-//               const SizedBox(
-//                 width: 5,
-//               ),
-//               Expanded(
-//                 child: TextField(
-//                   controller: viewModel.smsController,
-//                   // onChanged: (text) {
-//                   //   // viewModel.setNotifyListeners(); // Update the text status
-//                   // },
-//                   decoration: const InputDecoration(
-//                     hintText: 'Type your message...',
-//                     border: InputBorder.none,
-
-//                     // prefix: Icon(Icons.email, color: Colors.black,)
-//                   ),
-//                   maxLines: 5,
-//                   minLines: 1,
-//                 ),
-//               ),
-//               IconButton(
-//                 icon: Icon(Icons.send,
-//                     color: viewModel.smsController.text.isNotEmpty
-//                         ? const Color(0xff4873a6).withOpacity(0.7)
-//                         : Colors.grey),
-//                 onPressed: () {
-//                   if (viewModel.smsController.text.isNotEmpty) {
-//                     viewModel.sentSMS(viewModel.chatId, context,"");
-//                   }
-//                 },
-//               )
-//             ],
-//           ),
-//         ),
-//       ],
-//     ),
-//       viewModelBuilder: () => ChatPageViewModel(),
-//     );
-   
-//   }
-// }
