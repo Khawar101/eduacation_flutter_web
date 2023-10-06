0f53d99ad6338b2ad0d7d77e9f1c14c3d85f6bb8
Khawar101 <khawarjutt101@gmail.com>
2023-10-06 19:58:09 +0500

    nothing just check

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:developer';
-
-import 'package:education_flutter_web/services/profile_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:education_flutter_web/app/app.locator.dart';
