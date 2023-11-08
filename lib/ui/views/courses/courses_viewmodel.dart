a75d737e75c59009e0cfb42770fe376101e30cc0
Khawar101 <khawarjutt101@gmail.com>
2023-11-08 12:00:07 +0500

    course viewModel

--- lib/ui/views/courses/courses_viewmodel.dart
+++ lib/ui/views/courses/courses_viewmodel.dart
@@ -1,14 +1,14 @@
 // ignore_for_file: unused_import, unused_field
-import 'dart:developer';
 import 'package:education_flutter_web/ui/views/courses/upload_courses/widgets/pop_up_menu.dart';
 import 'package:education_flutter_web/utils/loading.dart';
-import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
+import '../../widgets/common/sized_text/sized_text.dart';
 import 'package:stacked_services/stacked_services.dart';
-import '../../../app/app.locator.dart';
 import '../../../services/Model/CoursesModel.dart';
 import '../../../services/courses_service.dart';
-import '../../widgets/common/sized_text/sized_text.dart';
+import '../../../app/app.locator.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'dart:developer';
 
 class CoursesViewModel extends BaseViewModel {
   // final _navigationService = locator<NavigationService>();
